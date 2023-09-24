2e45ab4a2b63391f0f768578009f7d5109034a40
Teto5000 <renzitochaves@hotmail.com>
2023-09-24 01:47:13 -0300

    plataforma movil lista

---
+++ TwinTrek2D/Assets/Scripts/Platforma.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platforma : MonoBehaviour
+{
+    public Transform puntoA; //el punto A donde debe dirigirse
+    public Transform puntoB; //el punto B donde debe dirigirse
+    [SerializeField] private LayerMask jugadorPlayer;
+    public float velocidad = 2.0f; //la velocidad con la que se mueve la plataforma
+    private Vector3 siguienteDestino; //representa al destino donde debe dirigirse la plataforma
+    private bool mover = false; //bandera que permite el movimiento o no
+
+    private void Start()
+    {
+        siguienteDestino = puntoB.position; //inicia con un denstino al principio
+    }
+
+    private void Update()
+    {
+        if (mover == true) //pregunta si debe moverse al siguiente destino
+        {
+            MoverPlataforma();
+        }
+        else //vuelve a donde se encontraba
+        {
+            MoverPlataformaInverso();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            siguienteDestino = puntoB.position;
+            mover = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            siguienteDestino = puntoA.position;
+            mover = false;
+        }
+    }
+    private void MoverPlataforma() //mueve la plataform al destino
+    {
+        transform.position = Vector3.MoveTowards(transform.position, siguienteDestino, velocidad * Time.deltaTime);
+    }
+
+    private void MoverPlataformaInverso() //mueve la plataforma al comienzo
+    {
+        siguienteDestino = puntoA.position;
+        transform.position = Vector3.MoveTowards(transform.position, siguienteDestino, velocidad * Time.deltaTime);
+    }
+}
