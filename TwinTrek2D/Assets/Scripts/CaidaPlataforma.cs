282cfc70e50f19fcba95e2a632d2df9e5ac8c6e9
Teto5000 <renzitochaves@hotmail.com>
2023-09-24 03:22:46 -0300

    plataforma que cae listo

--- TwinTrek2D/Assets/Scripts/CaidaPlataforma.cs
+++ TwinTrek2D/Assets/Scripts/CaidaPlataforma.cs
@@ -4,5 +4,37 @@ using UnityEngine;
 
 public class CaidaPlataforma : MonoBehaviour
 {
-    
+    private int velocidadY = -10; // Ajusta la velocidad Y según tus necesidades
+    [SerializeField] private bool parado = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Player2"))
+        {
+            parado = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (parado == true)
+        {
+            StartCoroutine(Caida());
+        }
+    }
+   
+
+    private void Caer()
+    {
+        float movimientoY = velocidadY * Time.deltaTime;
+        transform.Translate(0, movimientoY, 0);
+
+    }
+
+    IEnumerator Caida()
+    {
+        yield return new WaitForSeconds(2f);
+        Caer();
+    }
 }
+
