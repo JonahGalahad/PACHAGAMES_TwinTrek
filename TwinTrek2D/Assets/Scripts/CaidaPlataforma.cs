aceb49482de73492515c9aa8388ac804216973d5
Teto5000 <renzitochaves@hotmail.com>
2023-09-25 01:35:09 -0300

    asdasd

--- TwinTrek2D/Assets/Scripts/CaidaPlataforma.cs
+++ TwinTrek2D/Assets/Scripts/CaidaPlataforma.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class CaidaPlataforma : MonoBehaviour
 {
     private int velocidadY = -10; // Ajusta la velocidad Y según tus necesidades
+    private int velocidadY2 = 10;
     [SerializeField] private bool parado = false;
+    [SerializeField] private bool subiendo = false;
+    public Transform puntoA;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Player2"))
         {
             parado = true;
+        }
+      
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            subiendo = true;
         }
     }
 
@@ -20,6 +32,11 @@ public class CaidaPlataforma : MonoBehaviour
         if (parado == true)
         {
             StartCoroutine(Caida());
+        }
+
+        if (subiendo == true)
+        {
+            StartCoroutine(Subida());
         }
     }
    
@@ -31,10 +48,92 @@ public class CaidaPlataforma : MonoBehaviour
 
     }
 
+    private void Subir()
+    {
+        float movimientoY = velocidadY2 * Time.deltaTime;
+        transform.Translate(0, movimientoY, 0);
+
+    }
+
     IEnumerator Caida()
     {
         yield return new WaitForSeconds(2f);
         Caer();
+        yield return new WaitForSeconds(1f);
+        parado = false;
+    }
+
+    IEnumerator Subida()
+    {
+        yield return new WaitForSeconds(2f);
+        Subir();
+        yield return new WaitForSeconds(1f);
+        subiendo = false;
     }
 }
 
+/*private int velocidadY = -10; // Ajusta la velocidad Y según tus necesidades
+    private int velocidadY2 = 10;
+    [SerializeField] private bool parado = false;
+    [SerializeField] private bool subiendo = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Player2"))
+        {
+            parado = true;
+        }
+      
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            subiendo = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (parado == true)
+        {
+            StartCoroutine(Caida());
+        }
+
+        if (subiendo == true)
+        {
+            StartCoroutine(Subida());
+        }
+    }
+   
+
+    private void Caer()
+    {
+        float movimientoY = velocidadY * Time.deltaTime;
+        transform.Translate(0, movimientoY, 0);
+
+    }
+
+    private void Subir()
+    {
+        float movimientoY = velocidadY2 * Time.deltaTime;
+        transform.Translate(0, movimientoY, 0);
+
+    }
+
+    IEnumerator Caida()
+    {
+        yield return new WaitForSeconds(2f);
+        Caer();
+        yield return new WaitForSeconds(1f);
+        parado = false;
+    }
+
+    IEnumerator Subida()
+    {
+        yield return new WaitForSeconds(2f);
+        Subir();
+        yield return new WaitForSeconds(1f);
+        subiendo = false;
+    }*/
