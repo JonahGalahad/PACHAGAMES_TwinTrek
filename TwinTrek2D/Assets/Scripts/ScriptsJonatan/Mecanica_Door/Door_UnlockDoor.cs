3740754638e2b026217a261ec3ba2addf65819b2
Maximiliano Fabrizio Vega <mvnm270@gmail.com>
2024-11-15 13:39:19 -0300

    algo

--- TwinTrek2D/Assets/Scripts/ScriptsJonatan/Mecanica_Door/Door_UnlockDoor.cs
+++ TwinTrek2D/Assets/Scripts/ScriptsJonatan/Mecanica_Door/Door_UnlockDoor.cs
@@ -7,10 +7,23 @@ public class Door_UnlockDoor : MonoBehaviour
     [SerializeField] private Door_Controller keys; //Colocar el GameObject donde esta el script correspondiente
     [SerializeField] private int condNumKeys = 0; //numero de llaves necesaria para desbloquear puerta
     private bool isInDoor = false;  //flag si esta en rango para interactuar con la puerta
-    private Animator anim; 
+    //private Animator anim; 
+
+    [SerializeField] private GameObject key1;
+    [SerializeField] private GameObject key2;
+    [SerializeField] private GameObject key3;
 
     private void Start() {
-        anim = GetComponent<Animator>();
+        //anim = GetComponent<Animator>();
+        if (condNumKeys == 1)
+        {
+            key1.SetActive(true);
+            key2.SetActive(true);
+        }
+        else if (condNumKeys == 2)
+        {
+            key1.SetActive(true);
+        }
     }
     void Update()
     {
@@ -19,23 +32,29 @@ public class Door_UnlockDoor : MonoBehaviour
 
     // detecta si el jugador entra o sale del collider trigger de la puerta
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Player2")) {
+        if (other.gameObject.CompareTag("Player")) {
             isInDoor = true;
         }
     }
     private void OnTriggerExit2D(Collider2D other) {
-        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Player2")) {
+        if (other.gameObject.CompareTag("Player")) {
             isInDoor = false;
         }
     }
 
     // compara si la cantidad de llaves en posicion es >= a la cantidad de llaves necesarias para abrir la puerta, y abre la puerta
     private void OpenDoor() {
-        if (isInDoor==true  && Input.GetKeyDown(KeyCode.E) && keys.CurrentNumKeys>=condNumKeys) {
+        if (isInDoor==true  && (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.RightControl)) && keys.CurrentNumKeys>=condNumKeys)
+        {
             Debug.Log("Abrete sesamo");
             keys.CurrentNumKeys -= condNumKeys;
-            anim.SetBool("IsAnimStart", true);
-        } else if(isInDoor==true && Input.GetKeyDown(KeyCode.E) && keys.CurrentNumKeys!=condNumKeys) {
+            //anim.SetBool("IsAnimStart", true);
+            key3.SetActive(true);
+            key2.SetActive(true);
+            key1.SetActive(true);
+        }
+        else if(isInDoor==true && (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.RightControl)) && keys.CurrentNumKeys!=condNumKeys)
+        {
             Debug.Log("Aun no sesamo");
         }
     }
